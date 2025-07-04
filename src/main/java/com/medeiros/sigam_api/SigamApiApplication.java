4e4d905f6f74812001cf4f76b6f01618bc20efc2
DonboyBR <donboymedeiros@gmail.com>
2025-07-04 01:53:09 -0300

    Configuração inicial do projeto com Spring Boot e dependências básicas

---
+++ src/main/java/com/medeiros/sigam_api/SigamApiApplication.java
@@ -0,0 +1,13 @@
+package com.medeiros.sigam_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigamApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigamApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medeiros/sigam_api/SigamApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medeiros.sigam_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigamApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
